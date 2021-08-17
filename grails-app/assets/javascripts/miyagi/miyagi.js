2572f070049c4f8bc07b6ead9f2b280ca12195c1
Richard Rice <richardrice@Goodshuffles-MacBook-Pro.local>
2021-08-17 13:38:28 -0400

    init project with grails forge start.grails.org

---
+++ grails-app/assets/javascripts/miyagi/miyagi.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /miyagi/core/miyagi.core
+//= require /miyagi/index/miyagi.index
+
+angular.module("miyagi", [
+        "miyagi.core",
+        "miyagi.index"
+    ]);
---
+++ grails-app/init/miyagi/BootStrap.groovy
@@ -0,0 +1,9 @@
+package miyagi
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
