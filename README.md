f3b2f6231fc9f0b055de44311e1b803350462ea4
Richard Rice <richardrice@Goodshuffles-MacBook-Pro.local>
2021-08-18 11:51:10 -0400

    update readme, update logging levels, add test code to controller endpoints

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # miyagi
+
+Run with `grails run-app`
+
+## Troubleshooting
+If you see 
+```
+Execution failed for task ':findMainClass'.
+> Unable to find a single main class from the following candidates XYZ
+```
+
+Then run `./gradlew clean`
+
--- grails-app/controllers/miyagi/UserController.groovy
+++ grails-app/controllers/miyagi/UserController.groovy
@@ -1,11 +1,22 @@
 package miyagi
 
-
 import grails.rest.*
 import grails.converters.*
 
 class UserController {
-	static responseFormats = ['json', 'xml']
-	
-    def index() { }
+    static responseFormats = ['json']
+
+    def index() {
+        log.trace "PRINT TRACE"
+        log.debug "PRINT DEBUG"
+        log.info "PRINT INFO"
+        log.warn "PRINT WARNING"
+        log.error "PRINT ERROR"
+        respond new User(firstName:"Bob", lastName:"the Bulider")
+    }
+
+    def save() {
+        log.error "wow"
+        respond new User(firstName:"Darth", lastName:"Vader")
+    }
 }
