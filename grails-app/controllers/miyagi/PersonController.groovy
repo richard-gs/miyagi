90f8e6c892fb1f825eb3bf6ccb2cf1e59ce357cf
richard-gs <richard@goodshuffle.com>
2021-08-19 09:49:31 -0400

    finish up crud methods for person

--- grails-app/controllers/miyagi/PersonController.groovy
+++ grails-app/controllers/miyagi/PersonController.groovy
@@ -24,25 +24,32 @@ class PersonController {
      * curl -i -X POST -H "Content-Type: application/json" -d '{"name":"asdf", "firstName":"first!", "lastName":"last!"}' localhost:8080/person
      */
     def save(Person p) {
-        // https://docs.grails.org/3.3.11/guide/validation.html#validatingConstraints
-        if (p.validate()) {
-            p.save(flush: true, failOnError: true)
-            respond(['ok': p])
-        } else {
-            log.warn p.errors.toString()
-            response.sendError(400); // https://stackoverflow.com/questions/1429388/how-can-i-return-a-404-50x-status-code-from-a-grails-controller
-        }
+        saveUpdate(p)
     }
 
     // Test with:
     // curl -i -X PUT -H "Content-Type: application/json" -d '{"name":"asdf"}' localhost:8080/person/6
-    def update() {
-        log.info "update() params: ${params.toString()} | request: ${request.JSON}"
-        respond(['ok'])
+    def update(Person newPerson) {
+        Person existingPerson = Person.get(params.id)
+        if (!existingPerson) {
+            return response.sendError(404) 
+        }
+        existingPerson.properties = newPerson.properties
+        saveUpdate(existingPerson)
+    }
+
+    def saveUpdate(Person p) {
+        if (p.validate()) { // https://docs.grails.org/3.3.11/guide/validation.html#validatingConstraints
+            p.save(flush: true, failOnError: true)
+            respond(['ok': p])
+        } else {
+            log.warn p.errors.toString()
+            response.sendError(400) // https://stackoverflow.com/questions/1429388/how-can-i-return-a-404-50x-status-code-from-a-grails-controller
+        }
     }
 
     def delete() {
-        def p = Person.get(params.id);
+        def p = Person.get(params.id)
         p.delete(flush: true)
         respond(p)
     }
