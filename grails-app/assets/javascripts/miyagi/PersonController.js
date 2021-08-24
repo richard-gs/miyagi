c22662ddb911a05b949937f596519bee4cbd5fda
richard-gs <richard@goodshuffle.com>
2021-08-24 10:23:56 -0400

    add DC and puerto rico to states list

--- grails-app/assets/javascripts/miyagi/PersonController.js
+++ grails-app/assets/javascripts/miyagi/PersonController.js
@@ -1,7 +1,7 @@
 (() => {
 
 let DialogController = function($scope, $mdDialog) {
-	$scope.states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
+	$scope.states = ['AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','PR','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
 	$scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 	$scope.days = getDays();
 
